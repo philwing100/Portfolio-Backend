2afb62156de1cb12f1b0f29deace128df15330ed
philwing100 <philwing100@gmail.com>
2025-03-22 21:56:20 -0500

    Fix vercel auth 8

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 120, // Set cookie lifespan (30 minutes)
     httpOnly: true,
-    secure: false, // ✅ Secure only in production 
-    sameSite: process.env.NODE_ENV === "production" ? "lax" : "lax",
+    secure: process.env.NODE_ENV === "production" ? true : false, // ✅ Secure only in production 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   },
 }));
 console.log(process.env.NODE_ENV);
